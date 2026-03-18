a084be6696c280d97ce4ec96475cfe0f12b1cf63
aijiaowang1 <aijiaowang000@gmail.com>
2026-03-18 10:28:22 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'mbeysnhe.cc',
+	'rfjndae.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
