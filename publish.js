cf6979a5411b5b0f664728e6fde174350b2241f9
aijiaowang1 <aijiaowang000@gmail.com>
2026-06-18 09:53:06 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'egapmmfg.cc',
+	'pduudrb.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
