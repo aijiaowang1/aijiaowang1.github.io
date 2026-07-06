2e6663f6686e657daebace5e41b94db4a799f613
aijiaowang1 <aijiaowang000@gmail.com>
2026-07-06 09:06:03 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'yyiejwave.com',
+	'ztdybtmo.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
