2c7e04ba8cf6cb543fc3cfd4ac43d575f6edf554
aijiaowang1 <aijiaowang000@gmail.com>
2026-08-08 10:38:58 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'tnmuodihk.com',
+	'mkuolpnww.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
