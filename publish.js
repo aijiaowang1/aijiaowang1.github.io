fb90d841154182723d1dee588f42a7c756845a1f
aijiaowang1 <aijiaowang000@gmail.com>
2026-08-23 09:31:53 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'tdogwglja.com',
+	'msbyhkey.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
