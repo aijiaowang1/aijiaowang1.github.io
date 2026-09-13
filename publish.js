fa50c6f5081ec7d938edce01b3304adb17163f93
aijiaowang1 <aijiaowang000@gmail.com>
2026-09-13 10:59:05 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'ftngyxek.cc',
+	'nhvaxgvy.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
