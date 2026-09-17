8cac7a39506106738a399b2947fabfc36eddf32d
aijiaowang1 <aijiaowang000@gmail.com>
2026-09-17 14:43:54 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'twumywfd.cc',
+	'pahgbtzby.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
