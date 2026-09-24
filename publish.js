a97cb45d091d1df8ff531ffccc3839d5e6c07a41
爱交网 <aijiaowang000@gmail.com>
2026-09-24 16:34:59 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'eedbsphl.com',
+	'ggoqvvbs.cc',
 ];
                                                                                                                   
 var JumpPage="https://aijiaox.com";
